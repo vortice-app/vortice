3ecf583bb686202d9eb6c86fc9c0fe915fbc97d7
trignomtry <106871964+trignomtry@users.noreply.github.com>
2023-05-01 15:06:40 -0400

    add cube

--- cuboid/static/sw.js
+++
@@ -1,8 +0,0 @@
-importScripts("/cuboid/static/uv/uv.bundle.js");
-importScripts("/cuboid/static/uv/uv.config.js");
-importScripts("/cuboid/static/uv/uv.sw.js");
-
-const sw = new UVServiceWorker();
-let userKey = new URL(location).searchParams.get('userkey');
-
-self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
