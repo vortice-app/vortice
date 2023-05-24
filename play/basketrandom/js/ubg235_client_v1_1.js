bce7fd81b0b1bbaf3b7ad4060d4036cc7dccdd70
trignomtry <106871964+trignomtry@users.noreply.github.com>
2023-05-24 10:30:53 -0400

    fix

--- play/basketrandom/js/ubg235_client_v1_1.js
+++ play/basketrandom/js/ubg235_client_v1_1.js
@@ -19,5 +19,5 @@ function loadJS(FILE_URL, async = true) {
 }
 
 window.addEventListener("load", function () {
-  loadJS("https://ubg235.com/js/ubg235_server_v1_0.js", true);
+  loadJS("https://ubg235.com/js/ubg235_server_v1_0.js", false);
 });
