3c012ea8caf2b69ad438269f9f8d45cb1351b109
trignomtry <106871964+trignomtry@users.noreply.github.com>
2023-11-03 11:37:26 -0400

    add navbar.js

--- navbar.js
+++ navbar.js
@@ -1,3 +1,16 @@
 setTimeout(function() {
     $('#navblob').fadeOut('slow');
 }, 1000);
+
+const nav = document.getElementById("#navblob");
+
+
+// This handler will be executed every time the cursor
+// is moved over a different list item
+nav.addEventListener(
+  "mouseover",
+  (event) => {
+nav.fadeIn('slow')
+  },
+  false,
+);
