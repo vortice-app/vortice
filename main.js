c102ae7ac649cfe86a5700755631a9e7f7af92c0
trignomtry <northruptrig@gmail.com>
2024-01-04 18:38:04 -0500

    Added working themes and Secret AI✨

--- main.js
+++ main.js
@@ -1,27 +1,53 @@
-var themeInput = document.getElementById("theme");
-themeInput.addEventListener("change", function(){
-    changeTheme(this.value);
-})
+
+document.addEventListener("DOMContentLoaded", function(){
+    var themeInput = document.getElementById("theme");
+    themeInput.addEventListener("change", function(){
+        changeTheme(this.value);
+    })
+
+
+const status = localStorage.getItem("status");
+if(status == "banned"){
+    document.write("<h1>Account Banned</h1>");
+} else if (status == "normal"){
+} else if (status == "golden"){
+const b = document.body;
+b.classList.remove("night", "frost", "ruby", "orange", "lemon", "grass", "winter", "royal", "christmas");
+b.classList.add("golden");
+
+} else{}
+
+
+
+
 
 const savedTheme = localStorage.getItem('theme');
-
 if (savedTheme) {
     changeTheme(savedTheme);
     themeInput.value = savedTheme;
+} else {
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+if (prefersDarkScheme.matches) {
+  changeTheme("night")
+} else {
+  changeTheme("frost")
 }
+themeInput.value = prefersDarkScheme.matches ? "night" : "frost";
+}
+
 function changeTheme(theme) {
     const body = document.body;
 
     // Remove existing theme classes
     body.classList.remove('frost', 'night', 'ruby', 'orange', 'lemon', 'grass', 'winter', 'royal', 'christmas');
-
     // Add the selected theme class
     body.classList.add(theme);
 
     localStorage.setItem('theme', theme);
 
 }
-
+});
 
 
 function AB() {
