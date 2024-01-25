7fc4a6915fca5861e612cbcb642a5027f5ca4be2
trignomtry <106871964+trignomtry@users.noreply.github.com>
2024-01-25 08:24:10 -0500

    Fix Sheild

--- sheild/m/config.js
+++ sheild/m/config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/sheild/m/',
-    bare: 'https://mathplayground.cloud/v/',
+    bare: 'https://sunburst.sun.2of1.org/v/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/m/handler.js',
